37fb9a8662f91d45d246eb5552936df977f94286
Neeraj1702 <sandhuneeraj002@gmail.com>
2025-08-19 20:35:40 +0530

    add Task1.py to github

---
+++ Assignments/Task1.py
@@ -0,0 +1,14 @@
+# Perform Basic Mathematical Operations
+
+x=int(input("Enter First Number : "))
+y=int(input("Enter Second Number : "))
+
+add=x+y
+sub=x-y
+mul=x*y
+div=x/y
+
+print("\nAddition : ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",mul)
+print("Division :",div)
